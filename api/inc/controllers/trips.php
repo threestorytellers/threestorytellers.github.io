84f2691cea6155758fc2bdc71b53fc65039f1a46
wenuka <wenuka@gmail.com>
2020-05-24 14:44:32 +0200

    with api loading multiple, specific vehicle type

--- api/inc/controllers/trips.php
+++ api/inc/controllers/trips.php
@@ -8,5 +8,5 @@ define('APP_FOLDER_PATH', dirname(__FILE__) . '/../..');
 include(APP_FOLDER_PATH . '/inc/init.php');
 
 //$trip_rows = GTFS::getTripsByMinute($_GET['hhmm'], $_GET['ymd']);
-$trip_rows = GTFS::getSpecificTripsByMinute($_GET['hhmm'], $_GET['ymd'],400);
+$trip_rows = GTFS::getSpecificTripsByMinute($_GET['hhmm'], $_GET['ymd'],'(700)');
 JsonView::dump($trip_rows);
